720245e6087839004dcb82d06603ac5a8f7caf73
Viven Puthenpurayil <viven@Vivens-MacBook-Pro.local>
2024-02-24 00:26:52 -0600

    Making the defualt for ride_status and hopper_status 0

--- hopin/backend/models.py
+++ hopin/backend/models.py
@@ -12,7 +12,7 @@ class Trip(models.Model):
     driver_id = models.ForeignKey(User, related_name='driven_trips', on_delete=models.CASCADE)
     hoppers = models.ManyToManyField(User, related_name='hopped_trips', blank=True) 
     ride_status = models.IntegerField() # 0: posted, 1: in-progress, 2: done
-    date = models.DateField()
+    date = models.DateField(default=0)
     start_time = models.TimeField()
     end_time = models.TimeField()
     pickup_latitude = models.DecimalField(max_digits=9, decimal_places=6)
@@ -28,7 +28,7 @@ class Trip(models.Model):
 class HopperRequest(models.Model):
     trip_id = models.ForeignKey(Trip, related_name='trips_hopper_requests', on_delete=models.CASCADE)
     hopper_id = models.ForeignKey(User, related_name='hoppers_hopper_requests', on_delete=models.CASCADE)
-    hopper_status = models.IntegerField() # 0: requested, 1: accepted, 2: rejected
+    hopper_status = models.DateField(default=0) # 0: requested, 1: accepted, 2: rejected
 
     def __str__(self):
         return f"Hopper {self.hopper_id} for Trip {self.trip_id} - {self.get_hopper_status_display()}"
