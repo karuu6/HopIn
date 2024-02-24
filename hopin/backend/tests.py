03bd24c2cec2bff8ef420b6b396c2713f0e7286f
Viven Puthenpurayil <viven@Vivens-MacBook-Pro.local>
2024-02-24 15:54:52 -0600

    Removing unused imports

--- hopin/backend/tests.py
+++ hopin/backend/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, Client
+from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Trip, Profile
