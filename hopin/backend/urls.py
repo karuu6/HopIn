409289fcfb2e9c952dcf57ee34e670b3349df923
Kartik Subramanian <karu@Kartiks-MacBook-Pro-3.local>
2024-02-24 15:15:50 -0600

    wire in jwt

--- hopin/backend/urls.py
+++ hopin/backend/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("past_drives", views.past_drives, name="past_drives"),
-    path("past_hops", views.past_hops, name="past_hops"),
+    path("past_drives", views.PastDrives.as_view(), name="past_drives"),
+    path("past_hops", views.PastHops.as_view(), name="past_hops"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
