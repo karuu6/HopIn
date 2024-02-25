99d73f5c6101c67add2c64e90394b6c3065f94e7
Dhruv Sundararaman <dhruvsundararaman@Dhruvs-Laptop.local>
2024-02-25 00:48:30 -0600

    lebtronnnnn

--- hopin/backend/test_req.py
+++ hopin/backend/test_req.py
@@ -2,14 +2,68 @@ import requests
 
 headers = {
     'Content-Type': 'application/json;',
-    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4ODI2OTEzLCJpYXQiOjE3MDg4MjY2MTMsImp0aSI6ImIwMWUwY2UwNjU5MzRhNjQ4ZWNiYTQ5MzllMjkxYWQzIiwidXNlcl9pZCI6M30.zdDbM3mrrSRo2wi5QChS5vUqT3wfp8BxlSlkc0ReW78'
+    'Authorization': 'Bearer '
 }
 
-data = {
-    'trip_id': 1,
+signup_data = {
+    'username': '',
+    'password': '',
 }
 
-r = requests.post('http://127.0.0.1:8000/api/post_hopper_request/', json=data, headers=headers)
-print(r.status_code)
+login_data = {
+    'username': '',
+    'password': '',
+}
 
-print(r.json())
+trip_data1 = {
+    'date': '2024-03-10',
+    'start_time': '11:00:00',
+    'pickup_location': 'Champaign, IL',
+    'dropoff_location': 'Naperville, IL',
+    'open_seats': 3,
+    'price': 20,
+}
+
+trip_data2 = {
+    'date': '2024-03-10',
+    'start_time': '15:00:00',
+    'pickup_location': 'Savoy, IL',
+    'dropoff_location': 'Evanston, IL',
+    'open_seats': 1,
+    'price': 25,
+}
+
+rider = 'john'
+driver_names = ['shethh', 'prithvii']
+
+trip_ids = []
+for name, trip in zip(driver_names, [trip_data1, trip_data2]):
+    signup_json = signup_data.copy()
+    signup_json.update({'username': name, 'password': name})
+    signup_resp = requests.post('http://127.0.0.1:8000/api/signup/', json=signup_json).json()
+
+    login_json = signup_json
+    login_resp = requests.post('http://127.0.0.1:8000/api/token/', json=login_json).json()
+    token = login_resp['access']
+
+    cur_headers = headers.copy()
+    cur_headers['Authorization'] += token
+    post_ride_resp = requests.post('http://127.0.0.1:8000/api/post_trip/', json=trip, headers=cur_headers).json()
+
+    trip_ids.append(post_ride_resp['id'])
+
+signup_json = signup_data.copy()
+signup_json.update({'username': rider, 'password': rider})
+signup_resp = requests.post('http://127.0.0.1:8000/api/signup/', json=signup_json).json()
+
+login_json = signup_json
+login_resp = requests.post('http://127.0.0.1:8000/api/token/', json=login_json).json()
+token = login_resp['access']
+
+cur_headers = headers.copy()
+cur_headers['Authorization'] += token
+
+req_ride_json = {'trip_id': trip_ids[0]}
+req_ride_resp = requests.post('http://127.0.0.1:8000/api/post_hopper_request/', json=req_ride_json, headers=cur_headers).json()
+
+print(req_ride_resp)
